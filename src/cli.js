1072d4292c65e9363c2d778c574c50995ab7ff8f
Maxim Vernigorov <maxi.vernigorov@gmail.com>
2024-07-31 12:13:34 +0300

    .

--- src/cli.js
+++ src/cli.js
@@ -1,8 +1,6 @@
+import readlineSync from 'readline-sync';
 var readlineSync = require('readline-sync');
-
-const showGreeting = () => {
+export const showGreeting = () => {
 var userName = readlineSync.question('May I have your name? ');
 console.log('Hi ' + userName + '!');
 };
-
-export default showGreeeting;
