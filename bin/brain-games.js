38b1d3df0a827b7b39b74acc14b5ede6a85ee714
Maxim Vernigorov <maxi.vernigorov@gmail.com>
2024-08-04 14:58:21 +0300

    1

--- bin/brain-games.js
+++ bin/brain-games.js
@@ -1,5 +1,5 @@
 #!/usr/bin/env node
-import showGreeeting from '/home/maximvernigorov/frontend-project-44/src/cli.js';
+import showGreeeting from '../src/cli.js';
 console.log('Welcome to the Brain Games!');
 showGreeeting();
 
